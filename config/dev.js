05d79f6f1510ab38a7d429e29e06947a9519151b
Uday surya Bejjala <udaysuryabejjala@Udays-MacBook-Pro.local>
2026-01-19 00:22:31 +0530

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://udaysuryabejjala_db_user:miwltvmnHnICEAy5@cluster0.xqvz4if.mongodb.net/?appName=Cluster0&retryWrites=true&w=majority",
+  cookieKey: "123123123",
+};
