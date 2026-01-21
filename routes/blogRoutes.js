5d258d56fa618e9e935f720e5e68ead0790a7c5c
Uday surya <udaysurya1597@gmail.com>
2026-01-21 22:51:04 +0530

    added promisify

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,6 +14,21 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.01:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+    
+    //do we have any cached data in redis for this query
+    const cahchedBlogs = await client.get(req.user.id);
+
+    //if yes, then respond to the request right away and return
+
+
+    //if no, we need to respond to the request and update our cached data
+
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
