642b6dee2311e8baa30018497cbccefe146fb080
Uday surya <udaysurya1597@gmail.com>
2026-01-25 01:46:27 +0530

    Final hash key cache

--- services/cache.js
+++ services/cache.js
@@ -3,12 +3,13 @@ const redis = require('redis');
 const redisUrl = 'redis://127.0.01:6379';
 const client = redis.createClient(redisUrl);
 const util = require('util');
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
     this.cacheable = true;
+    this.hashKey = JSON.stringify(options.key || '');
     return this;
 }
 
@@ -18,32 +19,37 @@ mongoose.Query.prototype.exec = async function () { //arrow function is not used
             .apply(this, arguments);
     }
     else {
-       // console.log('IM ABOUT TO RUN A QUERY');
-    // console.log(this.getQuery());
-    // console.log(this.mongooseCollection.name);
-    const key=JSON.stringify(Object.assign({}, this.getQuery(), {
-        collection: this.mongooseCollection.name
-    }));
-    // console.log(key);
+        // console.log('IM ABOUT TO RUN A QUERY');
+        // console.log(this.getQuery());
+        // console.log(this.mongooseCollection.name);
+        const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+            collection: this.mongooseCollection.name
+        }));
+        // console.log(key);
 
-    //see if the key has any value in redis
-    const cachedValue = await client.get(key);
+        //see if the key has any value in redis
+        const cachedValue = await client.hget(this.hashKey, key);
 
-    //if do return that value
-    if (cachedValue) {
-        const doc = JSON.parse(cachedValue);
+        //if do return that value
+        if (cachedValue) {
+            const doc = JSON.parse(cachedValue);
 
-        return Array.isArray(doc) ?
-            doc.map(d => new this.model(d)) :
-            new this.model(doc);
+            return Array.isArray(doc) ?
+                doc.map(d => new this.model(d)) :
+                new this.model(doc);
+        }
+
+        //else return the data from mongo and set the key with value
+        const result = await exec.apply(this, arguments); //here this has control on Query that gets produced
+
+        client.hset(this.hashKey, key, JSON.stringify(result));
+
+        return result;
     }
+};
 
-    //else return the data from mongo and set the key with value
-    const result = await exec.apply(this, arguments); //here this has control on Query that gets produced
-
-    client.set(key, JSON.stringify(result));
-
-    return result; 
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
     }
-    
 }
