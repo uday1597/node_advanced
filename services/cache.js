b9e4b863635776fff4dad49d863ea8b4ecebcb63
Uday surya <udaysurya1597@gmail.com>
2026-01-25 00:55:18 +0530

    cached file changes

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://127.0.01:6379';
+const client = redis.createClient(redisUrl);
+const util = require('util');
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () { //arrow function is not used as to refer exec instance on this at line 8
-    console.log('IM ABOUT TO RUN A QUERY');
-    console.log(this.getQuery());
-    console.log(this.mongooseCollection.name);
+mongoose.Query.prototype.exec = async function () { //arrow function is not used as to refer exec instance on this at line 8
+    // console.log('IM ABOUT TO RUN A QUERY');
+    // console.log(this.getQuery());
+    // console.log(this.mongooseCollection.name);
+    const key=JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
+    // console.log(key);
 
-    return exec.apply(this, arguments); //here this has control on Query that gets produced
+    //see if the key has any value in redis
+    const cachedValue = await client.get(key);
+
+    //if do return that value
+    if (cachedValue) {
+        console.log(cachedValue);
+    }
+
+    //else return the data from mongo and set the key with value
+    const result = await exec.apply(this, arguments); //here this has control on Query that gets produced
+    console.log(result);
 }
