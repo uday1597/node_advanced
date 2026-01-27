3fd316121d9f4c95feb43d43003c6f33b8256dc2
Uday surya <udaysurya1597@gmail.com>
2026-01-27 20:27:39 +0530

    update mongo uri to local mongo compass uri

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://udaysuryabejjala_db_user:miwltvmnHnICEAy5@cluster0.xqvz4if.mongodb.net/?appName=Cluster0&retryWrites=true&w=majority",
+    "mongodb://localhost:27017/node",
   cookieKey: "123123123",
 };
