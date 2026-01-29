51a45b2971244d46c301c430711569b9f373141c
Uday surya <udaysurya1597@gmail.com>
2026-01-29 19:46:24 +0530

    blog post test

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -34,7 +34,7 @@ class CustomPage {
       { name: 'session.sig', value: sig, url: 'http://localhost:3000' }
   );
     
-  await this.page.goto('http://localhost:3000');
+  await this.page.goto('http://localhost:3000/blogs');
   await this.page.waitFor('a[href="/auth/logout"]');
   }
   
