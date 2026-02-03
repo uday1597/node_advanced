c14a4077d28bbb35f2492c6b936406e92bc47518
Uday surya <udaysurya1597@gmail.com>
2026-02-03 20:07:17 +0530

    Image integration.

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
   renderFields() {
     const { formValues } = this.props;
 
@@ -44,7 +45,12 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file,  history);
+  }
+
+  onFileChange(event) {
+    //aws pasword IamawierdP@ssword
+    this.setState({file: event.target.files[0]}); //[0] is added to get only one image in the uplaod however we can not select multiple images.n 
   }
 
   render() {
@@ -52,6 +58,12 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an Image</h5>
+        <input
+          onChange={this.onFileChange.bind(this)}
+          type="file"
+          accept="image/*" />
 
         {this.renderButtons()}
       </form>
